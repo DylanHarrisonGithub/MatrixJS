598eed5de75b4f544443b009055dee60305f9d62
Dylan Harrison <dillysnaps@gmail.com>
2018-10-16 21:07:12 -0500

    proof of concept

--- MatrixJS.js
+++ MatrixJS.js
@@ -1,38 +1,38 @@
 function Vector(v) {
 
-	this.v = [];
-	for (var i = 0; i < v.length; i++) {
-		this.v.push({
+    this.v = [];
+    for (var i = 0; i < v.length; i++) {
+        this.v.push({
             'f': v[i], 
             'string': '(' + v[i].toString().match(/return\s*(.*?)\s*;/)[1] + ')'}); //match return statement
-	}
+    }
 
     //dot product
-	this.dot = function(v2) {
-		if (v2.v.length == this.v.length) {
+    this.dot = function(v2) {
+        if (v2.v.length == this.v.length) {
             var s = function() { return 0; };
             var sString = '(';
-			for (var i = 0; i < this.v.length; i++) {
-				s = this._addProductToFunction(s, this.v[i].f, v2.v[i].f);
+            for (var i = 0; i < this.v.length; i++) {
+                s = this._addProductToFunction(s, this.v[i].f, v2.v[i].f);
                 sString += this.v[i].string + v2.v[i].string;
                 if (i < this.v.length-1) {
                     sString += ' + ';
                 }
             }
             sString += ')';
-			return {'f': s, 'string': sString};
-		} else {
-			return null;
-		}
-	};
+            return {'f': s, 'string': sString};
+        } else {
+            return null;
+        }
+    };
 
-	this.toString = function() {
-		var s = '[ ';
-		for (var i = 0; i < this.v.length; i++) {
-			s += this.v[i].string + ' ';
-		}
-		s += ']';
-		return s;		
+    this.toString = function() {
+        var s = '[ ';
+        for (var i = 0; i < this.v.length; i++) {
+            s += this.v[i].string + ' ';
+        }
+        s += ']';
+        return s;		
     }
     
     //return new function f1 + f2*f3
